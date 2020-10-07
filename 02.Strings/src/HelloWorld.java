551328319a6c9afa4e54435ea2e39079eff17bbc
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-07 17:57:53 +0530

    Strings

---
+++ 02.Strings/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		String firstName = "Hello ";
+	    String lastName = "Java";
+	    String fullName = firstName + lastName;
+	    System.out.println(fullName);  
+
+	}
+
+}
