2978571ddd420dc7309739d0fcfb6fd11b0e9b07
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-08 20:00:27 +0530

    Input

---
+++ 03.InputData/src/IOData.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IOData {
+
+	public static void main(String[] args) 
+	{
+        Scanner x = new Scanner(System.in);
+		
+        System.out.print("Enter a number: ");
+
+         
+        int number = x.nextInt();
+
+ 
+        System.out.println("You entered: " + number);
+
+	}
+
+}
