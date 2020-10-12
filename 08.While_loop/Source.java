6b0fd7c0ba8f88d039c5201221ea4e5a53eaf5e4
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-12 18:44:39 +0530

    While Loop

---
+++ 08.While_loop/Source.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Source {
+
+	    public static void main(String[] args) {
+        int i = 0;
+		while (i < 101) {
+		  System.out.println(i);
+		  i++;
+		}
+    }
+}
