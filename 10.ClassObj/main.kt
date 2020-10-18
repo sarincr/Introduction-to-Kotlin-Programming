1b579b2d36fcfba8577c3cdb12538f492974a2b7
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-18 20:22:42 +0530

    Class Objects

---
+++ 10.ClassObj/main.kt
@@ -0,0 +1,13 @@
+class myClass 
+{
+  private var X: String = "Kotlin Class"
+  fun printfun() 
+  {
+  print("Hello"+X)
+  }
+}
+fun main(args: Array<String>) 
+{
+   val obj = myClass()
+   obj.printfun()
+}
