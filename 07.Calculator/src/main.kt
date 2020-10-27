dedf22f4cb4f36bd9534e8ec45956145204b7944
Sarin CR <sarincrg@gmail.com>
2020-10-27 19:36:26 +0530

    Kotlin

---
+++ 07.Calculator/src/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var x: Int = 1
+    when(x)
+    {
+        1 -> print("x is one")
+        2 -> print("x is two")
+        else -> 
+        {
+            print("x is not good")
+        }
+    }
+}
