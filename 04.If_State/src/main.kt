6aba662eb6de9552b05904f19381b4812322df80
Sarin CR <sarincrg@gmail.com>
2020-10-27 16:12:24 +0530

    Methods and Types

---
+++ 04.If_State/src/main.kt
@@ -0,0 +1,11 @@
+
+fun main() { 
+        var a = 5
+        var b = 10
+        if(a > b){ 
+            print("Number 5 is larger than 10") 
+        } 
+        else{ 
+            println("Number 10 is larger than 5") 
+        } 
+    } 
---
+++ 05.EmployeeData/src/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    println("Name :")
+    val score = readLine()
+    println("Name = $score")
+    
+    
+    print("Phone Number: ")
+    val number = Integer.valueOf(readLine())
+    println("The entered number is: $number")
+}
